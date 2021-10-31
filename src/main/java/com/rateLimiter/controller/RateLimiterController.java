83622904886f671f00aa225e41de26f5cc094e6b
nikita01021998 <nikitakumari01021998@gmail.com>
2021-10-31 12:01:04 +0530

    Refactor

--- src/main/java/com/rateLimiter/controller/RateLimiterController.java
+++ src/main/java/com/rateLimiter/controller/RateLimiterController.java
@@ -2,7 +2,6 @@ package com.rateLimiter.controller;
 
 import com.rateLimiter.factory.RateLimiterFactory;
 import com.rateLimiter.pojo.BaseResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
